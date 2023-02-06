4c6d74498a84737d2612ab9f43a61a0c33fbfb9e
tomcsojn <44970244+tomcsojn@users.noreply.github.com>
2023-02-06 20:41:13 +0100

    Update dlib_test.py

--- dlib_test.py
+++ dlib_test.py
@@ -1,37 +1,65 @@
 from dlib_handler import Dlib_handler
 import cv2
 
-handler = Dlib_handler(2,0)
+def GUI_test():
 
-cam1 = cv2.VideoCapture(2)
-cam2 = cv2.VideoCapture(0)
-cv2.namedWindow("rgb_cam")
-cv2.namedWindow("inf_cam")
+    handler = Dlib_handler(2,0)
     
+    cam1 = cv2.VideoCapture(2)
+    cam2 = cv2.VideoCapture(0)
+    cv2.namedWindow("rgb_cam")
+    cv2.namedWindow("inf_cam")
+        
+        
+    while True:
+        #Read rgb and infra camera frames
+        ret1, frame1 = cam1.read()
+        ret2, frame2 = cam2.read()
+        cv2.imshow("rgb_cam", frame1)
+        cv2.imshow("inf_cam", frame2)
+        if not ret1:
+            break
+        k = cv2.waitKey(1)
+        
+        if k % 256 == 27:
+            #pressed esc
+            print("Escape hit, closing...")
+            cam1.release()
+            cam2.release()
+            cv2.destroyAllWindows()
+            break
+        elif k % 256 == 32:
+            identity =  handler.ID(frame1)
+            if identity:
+                print("identified as {}".format(identity))
+            else:
+                print("no user identified")
+            
+            
+def headless_test():
+    handler = Dlib_handler(2,0)
+    cam1 = cv2.VideoCapture(2)
+    cam2 = cv2.VideoCapture(0)
     
-while True:
-    #Read rgb and infra camera frames
-    ret1, frame1 = cam1.read()
-    ret2, frame2 = cam2.read()
-    cv2.imshow("rgb_cam", frame1)
-    cv2.imshow("inf_cam", frame2)
-    if not ret1:
-        break
-    k = cv2.waitKey(1)
-    
-    if k % 256 == 27:
-        #pressed esc
-        print("Escape hit, closing...")
-        cam1.release()
-        cam2.release()
-        cv2.destroyAllWindows()
-        break
-    elif k % 256 == 32:
-        # cv2.imwrite(img_name, frame1)
-        #move files to user path
+    while True:
+        #Read rgb and infra camera frames
+        ret1, frame1 = cam1.read()
+        ret2, frame2 = cam2.read()
+        # cv2.imshow("rgb_cam", frame1)
+        # cv2.imshow("inf_cam", frame2)
+        if not ret1:
+            break
+        k = cv2.waitKey(500)
+        
         identity =  handler.ID(frame1)
         if identity:
             print("identified as {}".format(identity))
         else:
             print("no user identified")
-            
+
+
+
+
+headless_test()
+# GUI_test()
+
